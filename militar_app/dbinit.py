e82c6c94793f407aa5b0c72a2e67bda2cb806cf1
Sérgio Fontes <79989206+sergiocpxfontes@users.noreply.github.com>
2023-05-16 21:18:54 +0100

    Update dbinit.py

--- militar_app/dbinit.py
+++ militar_app/dbinit.py
@@ -1,1 +1,3 @@
+from utilscls import Utils
+Utils.StarDatabase("CREATE TABLE [IF NOT EXISTS] militares (NIM TEXT,Patente TEXT,CC TEXT, Nome TEXT,Apelido TEXT, Nif TEXT, DataNascimento TEXT, Ramo TEXT);")
 
